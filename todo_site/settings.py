524a7ef8033bddefafc8749b73704f6b2b526e8a
Harsh <harshtreya11@gmail.com>
2026-01-12 19:49:52 +0530

    Change in django-todo for versal

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise'
+    'whitenoise.runserver_nostatic',
     'todo',
     'crispy_forms',
     'django.contrib.admin',
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
