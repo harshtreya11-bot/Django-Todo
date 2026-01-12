4df0635a39d74945c655b3dd69d7f4d075addc24
Harsh <harshtreya11@gmail.com>
2026-01-12 19:47:56 +0530

    Change in django-todo for versal

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise'
     'todo',
     'crispy_forms',
     'django.contrib.admin',
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS = [os.path.join(BASE_DIR, 'templates/todo')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
