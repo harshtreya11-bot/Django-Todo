5f7d8b58115f0f0d06d84021610cb92c48a89286
Harsh <harshtreya11@gmail.com>
2026-01-12 17:37:01 +0530

    my todo list

---
+++ todo/views.py
@@ -0,0 +1,33 @@
+"""Edit todo/views.py:
+
+index() shows the task list and handles new submissions.
+remove() deletes a task by ID.
+These views interact with the model and render templates with context data."""
+from django.shortcuts import render, redirect 
+from django.contrib import messages
+
+from .forms import TodoForm
+from .models import Todo
+
+# Create your views here.
+def index(request):
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('todo')
+    form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list" : item_list,
+        "title" : "TODO LIST",
+    }
+    return render(request, 'todo/index.html',page)
+def remove(request, item_id):
+    item = Todo.objects.get(id = item_id)
+    item.delete()
+    messages.info(request, "item removed successfully")
+    return redirect('todo')
+
